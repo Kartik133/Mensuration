0b4b7287d7d00b7b359f073879eb0d947ec8a434
Kartik133 <75470483+Kartik133@users.noreply.github.com>
2022-03-01 11:18:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
-var gameState = "select",selectShape,measurement,radius,heights,set,reset,x,y,cylinder;
+var gameState = "select",selectShape,measurement,radius,heights,lengths,slength,widths,set,reset,x,y;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
 
   radius = createInput("0","number");
   heights = createInput("0","number");
+  widths = createInput("0","number");
+  lengths = createInput("0","number");
+  slength = createInput("0","number");
   set = createButton("set");
   reset = createButton("reset");
   selectShape = createSelect();
@@ -35,32 +38,47 @@ function setup() {
   radius.position((width/2)-50,(height/2)-40);
   heights.position((width/2)-50,(height/2)-10);
   set.position((width/2)-50,(height/2)+20);
-  reset.position((width/2)-50,(height/2)+70);
+  reset.position((width/2)-50,(height/2)+20);
+  selectShape.position((width/2)-50,(height/2)-40);
+  measurement.position((width/2)-50,(height/2)-10);
 }
 
 function draw() {
   background(0);
+  console.log(gameState,"CylinderVolume");
 
   if(gameState==="select") {
     radius.hide();
     heights.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     reset.hide();
+    selectShape.show();
+    measurement.show();
 
-    set.mousePressed(()=>{
-      gameState = x;
+    set.mouseReleased(()=>{
+      var xxx = selectShape.value() + measurement.value()
+      gameState = xxx;
     });
-  }
+  }else {
+    selectShape.hide();
+    measurement.hide();
+   }
 
-  if(gameState==="cv") {
+  if(gameState==="CylinderVolume") {
     radius.show();
     heights.show();
     set.show();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     reset.hide();
    
     text("radius",(width/2)-100,(height/2)-20);
     text("height",(width/2)-100,(height/2)+10);
 
-    set.mousePressed(()=>{
+    set.mouseReleased(()=>{
       x = int(radius.value());
       y = int(heights.value());
       gameState = "cva";
@@ -70,28 +88,34 @@ function draw() {
   if(gameState==="cva"){
     radius.hide();
     heights.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     set.hide();
     reset.show();
 
     cylinderVol(x,y);
 
-    reset.mousePressed(()=>{
-      gameState = "cc";
+    reset.mouseReleased(()=>{
+      gameState = "CylinderVolume";
       radius.value(0);
       heights.value(0);
     });
   }
 
-  if(gameState==="cc") {
+  if(gameState==="CylinderC.S.A") {
     radius.show();
     heights.show();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     set.show();
     reset.hide();
    
     text("radius",(width/2)-100,(height/2)-20);
     text("height",(width/2)-100,(height/2)+10);
 
-    set.mousePressed(()=>{
+    set.mouseReleased(()=>{
       x = int(radius.value());
       y = int(heights.value());
       gameState = "cca";
@@ -102,27 +126,33 @@ function draw() {
     radius.hide();
     heights.hide();
     set.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     reset.show();
 
     cylinderCSA(x,y);
 
-    reset.mousePressed(()=>{
-      gameState = "ct";
+    reset.mouseReleased(()=>{
+      gameState = "CylinderC.S.A";
       radius.value(0);
       heights.value(0);
     });
   }
 
-  if(gameState==="ct") {
+  if(gameState==="CylinderT.S.A") {
     radius.show();
     heights.show();
     set.show();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     reset.hide();
 
     text("radius",(width/2)-100,(height/2)-20);
     text("height",(width/2)-100,(height/2)+10);
 
-    set.mousePressed(()=>{
+    set.mouseReleased(()=>{
       x = int(radius.value());
       y = int(heights.value());
       gameState = "cta";
@@ -133,12 +163,139 @@ function draw() {
     radius.hide();
     heights.hide();
     set.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
     reset.show();
 
     cylinderTSA(x,y);
 
-    reset.mousePressed(()=>{
-      gameState = "cv";
+    reset.mouseReleased(()=>{
+      gameState = "CylinderT.S.A";
+      radius.value(0);
+      heights.value(0);
+    });
+  }
+  
+  if(gameState==="ConeVolume") {
+    radius.show();
+    heights.show();
+    set.show();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
+    reset.hide();
+   
+    text("radius",(width/2)-100,(height/2)-20);
+    text("height",(width/2)-100,(height/2)+10);
+
+    set.mouseReleased(()=>{
+      x = int(radius.value());
+      y = int(heights.value());
+      gameState = "cova";
+    });
+  }
+  
+  if(gameState==="cova"){
+    radius.hide();
+    heights.hide();
+    set.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
+    reset.show();
+
+    coneVol(x,y);
+
+    reset.mouseReleased(()=>{
+      gameState = "ConeVolume";
+      radius.value(0);
+      heights.value(0);
+    });
+  }
+
+  if(gameState==="ConeC.S.A") {
+    radius.show();
+    heights.show();
+    widths.hide();
+    lengths.hide();
+    slength.show();
+    set.show();
+    reset.hide();
+   
+    text("radius",(width/2)-100,(height/2)-20);
+    text("height",(width/2)-100,(height/2)+10);
+
+    set.mouseReleased(()=>{
+      x = int(radius.value());
+      y = int(heights.value());
+      z = int(slength.value());
+      gameState = "coca";
+    });
+  }
+
+  if(gameState==="coca"){
+    radius.hide();
+    heights.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
+    set.hide();
+    reset.show();
+
+    console.log(slength.value(),z);
+
+    if(z !== 0) {
+      coneCSA(x,z);
+    }else {
+       coneCSAA(x,y);
+     }
+
+    reset.mouseReleased(()=>{
+      gameState = "ConeC.S.A";
+      radius.value(0);
+      heights.value(0);
+    });
+  }
+
+  if(gameState==="ConeT.S.A") {
+    radius.show();
+    heights.show();
+    widths.hide();
+    lengths.hide();
+    slength.show();
+    set.show();
+    reset.hide();
+
+    text("radius",(width/2)-100,(height/2)-20);
+    text("height",(width/2)-100,(height/2)+10);
+
+    set.mouseReleased(()=>{
+      x = int(radius.value());
+      y = int(heights.value());
+      z = int(slength.value());
+      gameState = "cota";
+    });
+  }
+
+  if(gameState==="cota"){
+    radius.hide();
+    heights.hide();
+    widths.hide();
+    lengths.hide();
+    slength.hide();
+    set.hide();
+    reset.show();
+
+    if(z !== 0) {
+      coneTSA(x,z);
+    }else {
+       coneTSAA(x,y);
+     }
+    
+
+    reset.mouseReleased(()=>{
+      gameState = "ConeT.S.A";
       radius.value(0);
       heights.value(0);
     });
@@ -148,35 +305,47 @@ function draw() {
 function cylinderVol(r,h) {
   noStroke();
   fill(255);
-  text(22/7*r*r*h,(width/2)-50,(height/2)+40);
+  text(22/7*r*r*h,(width/2)-50,height/2);
 }
 
 function cylinderCSA(r,h) {
   noStroke();
   fill(255);
-  text(2*22/7*r*h,(width/2)-50,(height/2)+40);
+  text(2*22/7*r*h,(width/2)-50,height/2);
 }
 
 function cylinderTSA(r,h) {
   noStroke();
   fill(255);
-  text(2*22/7*r*(r+h),(width/2)-50,(height/2)+40);
+  text(2*22/7*r*(r+h),(width/2)-50,height/2);
 }
 
 function coneVol(r,h) {
   noStroke();
   fill(255);
-  text(1/3*22/7*r*r*h,(width/2)-50,(height/2)+40);
+  text(1/3*22/7*r*r*h,(width/2)-50,height/2);
 }
 
 function coneCSA(r,l) {
   noStroke();
   fill(255);
-  text(22/7*r*l,(width/2)-50,(height/2)+40);
+  text(22/7*r*l,(width/2)-50,height/2);
 }
 
 function coneTSA(r,l) {
   noStroke();
   fill(255);
-  text(22/7*r*(r+l),(width/2)-50,(height/2)+40);
+  text(22/7*r*(r+l),(width/2)-50,height/2);
+}
+
+function coneCSAA(r,h) {
+  noStroke();
+  fill(255);
+  text(22/7*r*(sqrt((h*h+r*r))),(width/2)-50,height/2);
+}
+
+function coneTSAA(r,h) {
+  noStroke();
+  fill(255);
+  text(22/7*r*(r+(sqrt((h*h+r*r)))),(width/2)-50,height/2);
 }
